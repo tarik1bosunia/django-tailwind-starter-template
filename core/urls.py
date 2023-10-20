755d9d044dc896446846487e26709502f5a34aa5
md tarik bosunia <bosuniamdtarik005@gmail.com>
2023-10-20 18:34:50 +0600

    updated project setup

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ecommerse_django_tailwind project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,16 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls', namespace='home')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
-
-# Serve static and media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
